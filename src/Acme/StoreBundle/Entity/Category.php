dc1f192ea3d2990c87857943b6774b628a802ae5
B0cha <artbocha@gmail.com>
2016-01-24 15:33:09 +0300

    struct php to yaml

--- src/Acme/StoreBundle/Entity/Category.php
+++ src/Acme/StoreBundle/Entity/Category.php
@@ -2,24 +2,10 @@
 
 namespace Acme\StoreBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
-/**
-* @ORM\Entity
-* @ORM\Table(name="category")
-*/
 class Category
-{	
-	/**
-	* @ORM\Id
-	* @ORM\Column(type="integer")
-	* @ORM\GeneratedValue(strategy="AUTO")
-	*/
+{
 	protected $id;
 
-	/**
-	* @ORM\Column(type="string", length=40)
-	*/
 	protected $name;
 
     /**
