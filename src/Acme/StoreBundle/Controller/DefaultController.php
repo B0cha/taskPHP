644674318693339d6bccc178e41f6bf5d9379bc2
B0cha <artbocha@gmail.com>
2016-01-25 23:15:14 +0300

    create homes pages: 'site.com'

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -3,11 +3,41 @@
 namespace Acme\StoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Acme\StoreBundle\Entity\Category;
+use Acme\StoreBundle\Entity\Product;
+
+
+/**
+* @Route("/site.com")
+*/
 class DefaultController extends Controller
-{
+{	
+	/**
+	* @Route("/", name="home")
+	*/
     public function indexAction()
+    {	
+    	$em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('AcmeStoreBundle:Category')->findAll();
+        $products = $em->getRepository('AcmeStoreBundle:Product')->findAll();
+        return $this->render('site/home.html.twig', array(
+            'categories' => $categories,
+            'products' => $products
+        ));
+        //return $this->render('site/home.html.twig');
+    }
+
+    /**
+    * @Route("/{id}", name="site_category_name")
+    * @Method("GET")
+    */
+    public function showProductsCategoryAction(Category $category)
     {
-        return $this->render('AcmeStoreBundle:Default:index.html.twig');
+    	
     }
 }
